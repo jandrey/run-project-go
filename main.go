9c0afab301d2c37fa29a94abd6d9c58374f13fe9
jandrey <jandreyoliveira@gmail.com>
2025-08-16 19:40:57 -0300

    created mvc and configuration method error

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
 }
